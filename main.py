d69b93c9d51ab6e7f02368f4e7dff5bb98adb523
JK <jag-k58@ya.ru>
2018-03-06 18:32:04 +0300

    Большая задача по Maps API. Часть №1

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-import pygame
 import requests
+from multitool import pygame, search_spn, convert_bytes
+import sys
 
+ERROR_STRING = '\x1b[31;1mError\x1b[1m (\x1b[36;1m%s\x1b[1m): \x1b[4;1m%s\x1b[0m'
+FPS = 60
 size = 600, 450
+spn = 0, 0
+max_spn = 0, 0
+
+locate = "Красная площадь, 1"
 
 
-# CODE
+def get_coord(location, sco='longlat'):
+    url = "http://geocode-maps.yandex.ru/1.x/?geocode=%s&format=json&sco=%s" % (location, sco)
+    response = requests.get(url)
+    if response:
+        res = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+        return tuple(map(float, res.split()))
+
+
+def get_request(url, params=None, **kwargs):
+    try:
+        res = requests.get(url, params, **kwargs)
+        if not res:
+            print(ERROR_STRING % (res.status_code, res.reason))
+            sys.exit(res.status_code)
+        else:
+            return res
+    except Exception as err:
+        print(ERROR_STRING % (type(err).__name__, err))
+        sys.exit(1)
+
+
+def str_param(*params):
+    return ','.join(map(str, params))
+
+
+def map_image(long, lat, l=['sat', 'skl']):
+    url = 'https://static-maps.yandex.ru/1.x/'
+
+    params = {
+        "ll": str_param(long, lat),
+        "l": ','.join(l),
+        "size": str_param(*size),
+        "spn": str_param(*spn)
+    }
+
+    res = get_request(url, params)
+
+    return convert_bytes(res.content)
+
+
+image = map_image(*get_coord(locate))
 
 pygame.init()
-pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode(size)
+surface.blit(image, (0, 0))
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,5 +66,6 @@ while running:
             break
 
     pygame.display.flip()
+    clock.tick(FPS)
 
 pygame.quit()
---
+++ multitool.py
@@ -0,0 +1,15 @@
+import pygame
+from PIL import Image
+from io import BytesIO
+
+
+def search_spn(geo_object):
+    data = geo_object["boundedBy"]["Envelope"]
+    upper = tuple(map(float, data["upperCorner"].split()))
+    lower = tuple(map(float, data["lowerCorner"].split()))
+    return str(upper[0] - lower[0]), str(upper[1] - lower[1])
+
+
+def convert_bytes(bytes_string):
+    im = Image.open(BytesIO(bytes_string)).convert("RGBA")
+    return pygame.image.fromstring(im.tobytes(), im.size, im.mode)
